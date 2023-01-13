b6f21c286129c8ef670599672470f7759f1af2d0
Rajan <shrestharajan726@gmail.com>
2023-01-13 19:23:56 +0545

    arr

---
+++ 03LEC/Aliases/aliObject.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+function createUser(user) {
+    return { name: "", email: "", isActive: true };
+}
+createUser({ name: "", email: "", isActive: true });
+// function createUsers(user: Users){
+//     return {name: "", email: "", isActive: true, _id: 1}
+// }
+var myUser = {
+    name: "Rajan",
+    email: "nr@r",
+    isActive: false,
+    _id: 1
+};
---
+++ 04LEC/myArr.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+const allUsers: User[] = []
+
+allUsers.push({name: "Rajan", email: "nr@r", isActive: true})
+
+console.log(allUsers)
+
+const MLModels: number[][]=[
+    [1,2,3],
+    [4,5,6],
+]
+
+export {}
