a85e969fe858b7772a1e6a93868f7798afc82192
Rajan <shrestharajan726@gmail.com>
2023-01-13 19:11:05 +0545

    readonly, optional and &

--- 03LEC/Aliases/aliObject.ts
+++ 03LEC/Aliases/aliObject.ts
@@ -10,4 +10,40 @@ function createUser(user: User){
 
 createUser({name: "", email:"", isActive: true})
 
+
+
+//READONLY
+type Users = {
+    name: string;
+    email: string;
+    isActive: boolean;
+    readonly _id: number; //readonly property
+    credit?: number; //optional property
+}
+
+// function createUsers(user: Users){
+//     return {name: "", email: "", isActive: true, _id: 1}
+// }
+
+let myUser: Users ={
+    name: "Rajan",
+    email: "nr@r",
+    isActive: false,
+    _id: 1
+}
+
+//use of  &
+type User1 = {
+    cardNum: number;
+}
+type User2 = {
+    cardNum: number;
+}
+
+type User3 = User1 & User2 &{
+    cvv: number;
+};
+
+
+
 export {}
