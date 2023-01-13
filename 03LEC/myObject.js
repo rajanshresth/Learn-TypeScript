1202c41b5a31e687ef5684b3d930674ac51fdad3
Rajan <shrestharajan726@gmail.com>
2023-01-13 18:48:39 +0545

    03LEC Added

---
+++ 03LEC/myObject.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var User = {
+    name: "Rajan",
+    email: "shrestharajan726@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+    console.log();
+}
+var newUser = { name: "Rajan", isPaid: true };
+createUser({ name: "Rajan", isPaid: true });
+function createCourse() {
+    return { name: "React", price: 100 };
+}
---
+++ 03LEC/myObject.ts
@@ -0,0 +1,19 @@
+const User = {
+    name: "Rajan",
+    email: "shrestharajan726@gmail.com",
+    isActive: true
+}
+
+function createUser({name:string, isPaid:boolean}){
+    console.log()
+}
+
+let newUser = {name: "Rajan", isPaid: true}
+
+createUser({name: "Rajan", isPaid: true});
+
+function createCourse():{name: string, price:number}{
+    return {name: "React", price: 100}
+}
+
+export {} 
