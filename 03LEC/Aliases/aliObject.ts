5655e691fc4ee2a02b00a06cf4a1c62ed4928278
Rajan <shrestharajan726@gmail.com>
2023-01-13 19:00:37 +0545

    aliases

---
+++ 03LEC/Aliases/aliObject.ts
@@ -0,0 +1,13 @@
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+function createUser(user: User){
+    return {name: "", email: "", isActive: true}
+}
+
+createUser({name: "", email:"", isActive: true})
+
+export {}
